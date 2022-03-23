8676cc91bce75a9f6f97add968fa38d8435230fb
Kabelo Mighty Hlungwani <53508373+kabelo-mighty@users.noreply.github.com>
2022-03-24 00:49:28 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>VPS</title><link rel="icon" href="assets/img/carlogo.png">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/Article-Clean.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="assets/css/JLX-Fixed-Nav-on-Scroll.css">
+    <link rel="stylesheet" href="assets/css/Login-Box-En.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+    <link rel="stylesheet" href="assets/css/Social-Menu-Sticky.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/WOWSlider-about-us-1.css">
+    <link rel="stylesheet" href="assets/css/WOWSlider-about-us-2.css">
+    <link rel="stylesheet" href="assets/css/WOWSlider-about-us.css">
+</head>
+
+<body style="font-family: Montserrat, sans-serif;">
+    <nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
+        <div class="container"><a class="navbar-brand" href="#" style="color: rgb(50,211,56);">VPS&nbsp;<i class="fas fa-car-alt"></i></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav mr-auto">
+                    
+                   
+             
+                  
+                </ul><span class="navbar-text actions"> <a class="btn btn-light action-button" role="button" href="account/login.php" style="background: rgb(50,211,56); font-weight: 600;">Sign In</a></span>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="carousel slide" data-ride="carousel" data-pause="false" data-aos="fade-up-right" id="carousel-1" ">
+            <div class="carousel-inner" style="height:576px;">
+                <div class="carousel-item active"><img class="w-100 d-block" src="assets/img/car1.jpg" alt="Slide Image"></div>
+                <div class="carousel-item"><img class="w-100 d-block" src="assets/img/car2.jpg" alt="Slide Image"></div>
+            </div>
+            <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-slide="prev"><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-slide="next"><span class="sr-only">Next</span></a></div>
+        </div>
+   
+    
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+    <script src="assets/js/JLX-Fixed-Nav-on-Scroll.js"></script>
+    <script src="assets/js/WOWSlider-about-us.js"></script>
+</body>
+
+</html>
